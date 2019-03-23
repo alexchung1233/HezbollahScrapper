d65da2055785914103dc5096390cfefd82273a51
alexchung1233 <awchung@email.wm.edu>
2019-03-23 18:13:28 -0400

    Updated settings and configuration for Scrapper

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -16,22 +16,24 @@ NEWSPIDER_MODULE = 'tutorial.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/72.0.3626.121 Chrome/72.0.3626.121 Safari/537.36)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 75
+CONCURRENT_REQUESTS = 128
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+#DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
-
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_IP = 3
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1
+AUTOTHROTTLE_MAX_DELAY = 3
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
 
@@ -46,9 +48,10 @@ COOKIES_ENABLED = False
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'tutorial.middlewares.TutorialSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+    'tutorial.middlewares.DeltaFetch': 600
+}
+DELTAFETCH_ENABLED = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
@@ -92,4 +95,10 @@ COOKIES_ENABLED = False
 
 REDIRECT_ENABLED = False
 RETRY_ENABLED = False
-#LOG_LEVEL = 'INFO'
+#sLOG_LEVEL = 'INFO'
+#DEPTH_PRIORITY = 1
+#SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
+#SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
+#SCHEDULER_PRIORITY_QUEUE = 'queues.RoundRobinPriorityQueue'
+#SCHEDULER_DISK_QUEUE = 'queues.DiskQueue'
+#SCHEDULER = 'domain_scheduler.DomainScheduler'
--- tutorial/tutorial/spiders/webcrawler_spider.py
+++ tutorial/tutorial/spiders/webcrawler_spider.py
@@ -7,6 +7,7 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.item import Item
 from tutorial.items import TutorialItem
+import csv
 
 
 def find_all_substrings(string, sub):
@@ -20,19 +21,46 @@ def find_all_substrings(string, sub):
 class GenericSpider(CrawlSpider):
 
     name = "webcrawler"
-    allowed_domains = ["www.aljazeera.com","www.counterextremism.com","www.nytimes.com","www.theatlantic.com/international"]
-    start_urls = ["https://www.aljazeera.com/topics/organisations/hezbollah.html","https://www.counterextremism.com","https://www.nytimes.com/topic/organization/hezbollah","https://www.theatlantic.com/international/archive/2018/05/lebanon-election-hezbollah-sunni-shia/559772/"]
+    #allowed_domains =[url[0][8:] for url in csv.reader(open('C:/Users/Alex/Desktop/HezbollahScrapper-master/urls.csv','r'),delimiter =',')]
+    allowed_domains = ["www.dailystar.com.lb","www.counterextremism.com","www.bbc.com"]
+    #,"www.aljazeera.com","www.nytimes.com","www.theatlantic.com","www.thenational.ae/world","www.washingtonpost.com/world","www.bbc.com/news","www.presstv.com"]
+    #start_urls = ["https://www.aljazeera.com/topics/organisations/hezbollah.html","https://www.nytimes.com/topic/organization/hezbollah"
+    #            ,"https://www.theatlantic.com/international/archive/2018/05/lebanon-election-hezbollah-sunni-shia/559772/"
+    #            ,"https://www.thenational.ae/world/mena/us-warns-of-growing-hezbollah-influence-as-lebanon-nears-agreement-on-new-government-1.804342",
+    #            "https://www.washingtonpost.com/world/middle_east/hezbollah-on-the-rise-in-lebanon-fends-off-saudi-arabia/2017/11/23/d9d92b1c-c961-11e7-b506-8a10ed11ecf5_story.html?noredirect=on&utm_term=.f147561e9014",
+    #            "https://www.counterextremism.com",
+    #            "https://www.bbc.com/news/world-middle-east-10814698",
+    #            "https://www.presstv.com/Detail/2018/12/19/583358/Lebanon-US-Israel-Hezbollah-influence-political-system-war"]
+    #start_urls = [url[0] for url in csv.reader(open('C:/Users/Alex/Desktop/HezbollahScrapper-master/urls.csv','r'),delimiter =',')]
+    start_urls = ["http://www.dailystar.com.lb","http://www.counterextremism.com","https://www.aljazeera.com"]
 
     #possibly use process_links to to filter out links that dont mention hezbollah
     rules = [Rule(LinkExtractor(unique = True), follow=True, callback="check_buzzwords")]
 
-    #handles each individual request given to the parsing function
+    terms = []
+    locations = []
+    organizations = []
+    wordlist = []
+    with open('C:/Users/Alex/Desktop/HezbollahScrapper-master/terms.csv','r') as csvfile:
+        terms_reader = csv.reader(csvfile,delimiter = ',')
+        for row in terms_reader:
+            terms.append(row[0])
+            organizations.append(row[1])
+    for term in terms:
+        for indx in range(0,5):
+            wordlist.append(tuple((term,organizations[indx])))
+
+
+    def __init__(self, category=None, *args, **kwargs):
+        self.rules = [Rule(LinkExtractor(unique = True), follow=True, callback="check_words")]
+        super(GenericSpider, self).__init__(*args, **kwargs)
 
 
 
-    def check_buzzwords(self, response):
 
-        wordlist = ["hospital","mosque","clinic","charitable","Jihad al-Binaa","charitable","school","medical centers","al-Waad al-Sadiq"]
+
+
+    def check_words(self, response):
         url = response.url
         contenttype = response.headers.get("content-type", "").decode('utf-8').lower()
         items = []
@@ -41,10 +69,11 @@ class GenericSpider(CrawlSpider):
         p_texts = [p.get() for p in paragraph_text]
 
         for p_text in p_texts:
-            for word in wordlist:
-                if word in p_text and "Hezbollah" in p_text:
+            p_text_lower = p_text.lower()
+            for word_row in self.wordlist:
+                if word_row[0].lower() in p_text_lower and word_row[1].lower() in p_text_lower:
                     item = TutorialItem()
-                    item["word"] = word
+                    item["word"] = word_row[0]
                     item["url"] = url
                     item["sentence"] = p_text
                     items.append(item)
@@ -53,6 +82,14 @@ class GenericSpider(CrawlSpider):
                     #    item["sentence"] = found.group(0)
 
         return(items)
+    def parse_item(self, response):
+        print('!!!!!!!!!!!!! Parsing: %s !!!!!!!!!!!!!' % response.url)
+
+        # Check the Content-Type.
+        if is_content_type_ok(response.headers.getlist('Content-Type')):
+            # Yield data here
+            yield {}
+
 
     #gets the requests to follow recursively
     def _requests_to_follow(self, response):
